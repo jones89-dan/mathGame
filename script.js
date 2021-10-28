ce8310fac5f940db87a826d0795722d340bcef21
Daniel <jones.89.dan@gmail.com>
2021-10-27 21:06:06 -0700

    created function that injects new question if user input is corret

--- script.js
+++ script.js
@@ -26,15 +26,25 @@ $(document).ready(function(){
   currentQuestion = questionGenerator();
   $('#equation').text(currentQuestion.equation);
 
+  var renderNewQuestion = function () {
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);
+  }
+
  $('#user-input').on('keyup', function () {
    console.log($(this).val());
   });
 
   var checkAnswer = function (userInput, answer) {
-    console.log(userInput === answer);
+    if(userInput === answer) {
+      renderNewQuestion();
+       $('#user-input').val('');
+    }
   }
 
   $('#user-input').on('keyup', function () {
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
+
+  renderNewQuestion();
 });
