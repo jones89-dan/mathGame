8d1641fa0d6b9348167c4ac609c257b1e3268ca4
Daniel <jones.89.dan@gmail.com>
2021-10-27 20:58:34 -0700

    Created funciton to compare correct answer and user input

--- script.js
+++ script.js
@@ -10,20 +10,31 @@ $(document).ready(function(){
 
     if (correctAnswer == userAnswer)
       return true;
-  }
+    }
 
   var questionGenerator = function () {
-  var question = {};
-  var num1 = randomNumberGenerator(10);
-  var num2 = randomNumberGenerator(10);
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
 
-  question.answer = num1 + num2;
-  question.equation = String(num1) + " + " + String(num2);
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
 
-  return question;
-}
+    return question;
+  }
 
-currentQuestion = questionGenerator();
-$('#equation').text(currentQuestion.equation);
+  currentQuestion = questionGenerator();
+  $('#equation').text(currentQuestion.equation);
 
+ $('#user-input').on('keyup', function () {
+   console.log($(this).val());
+  });
+
+  var checkAnswer = function (userInput, answer) {
+    console.log(userInput === answer);
+  }
+
+  $('#user-input').on('keyup', function () {
+    checkAnswer(Number($(this).val()), currentQuestion.answer);
+  });
 });
