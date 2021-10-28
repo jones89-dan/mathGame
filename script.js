061240a3870d758753a5446d825b41c2b6a15752
Daniel <jones.89.dan@gmail.com>
2021-10-27 22:12:54 -0700

    updated score function to reset once time is up

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ $(document).ready(function(){
 
         if (timeLeft === 0) {
           updateTimeLeft(10);
+          updateScore(-score);
         }
         interval = setInterval(function () {
           updateTimeLeft(-1);
